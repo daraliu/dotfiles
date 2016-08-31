8acb1354203a8691f3707b860c39fd339d07e398
Darius Aliulis <darius.aliulis@gmail.com>
2016-08-31 09:19:37 +0300

    added docker aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -31,8 +31,23 @@ if command_exists docker-compose; then
 fi
 
 if command_exists docker; then
-    alias docker-remove-containers='docker rm $(docker ps -a -q)'
-    alias docker-remove-images='docker rmi $(docker images -q)'
+    # Kill all running containers.
+    alias dockerkillall='docker kill $(docker ps -q)'
+
+    # Delete all stopped containers.
+    alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+
+    # Delete all untagged images.
+    alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+
+    # Delete all untagged volumes
+    alias dockercleanv='printf "\n>>> Deleting untagged volumes\n\n" && docker volume ls -qf dangling=true | xargs -r docker volume rm'
+
+    # Delete all stopped containers and untagged images.
+    alias dockerclean='dockercleanc || true && dockercleani || true && dockercleanv'
+fi
+
+if command_exists docker; then
 fi
 
 if command_exists ffmpeg; then
