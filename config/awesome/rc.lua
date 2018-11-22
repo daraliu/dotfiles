ec7259bc372557d97ed3d95c7ea7616dba6d08bc
Darius Aliulis <darius.aliulis@gmail.com>
2018-11-22 12:48:01 +0200

    add solarized theme for awesome

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -38,8 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
-beautiful.wallpaper = awful.util.get_configuration_dir() .. "solarized_arch.png"
+beautiful.init(awful.util.get_configuration_dir().."themes/solarized_dark/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "termite"
