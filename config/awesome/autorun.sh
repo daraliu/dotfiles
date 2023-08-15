94efda9182de95fca88eb7c02c40517fb4809d0f
Darius Aliulis <darius.aliulis@gmail.com>
2023-08-15 22:27:01 +0300

    enable picom composer and drop compton

---
+++ config/awesome/autorun.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# See https://wiki.archlinux.org/title/Awesome#Autostart
+
+run() {
+  if ! pgrep -f "$1" ;
+  then
+    "$@"&
+  fi
+}
+
+exec picom --experimental-backends --config ~/.config/picom/picom.conf &
+
--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -562,4 +562,7 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- See https://wiki.archlinux.org/title/Awesome#Autostart
+awful.spawn.with_shell("~/.config/awesome/autorun.sh")
 -- }}}
