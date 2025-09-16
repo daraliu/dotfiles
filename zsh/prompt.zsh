2a4009a3d2027c5349b2c5a0ecce8729ecb49a77
Darius Aliulis <darius.aliulis@satalia.com>
2025-09-16 14:19:57 +0300

    add, but comment out cowsay random quotes

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -121,3 +121,5 @@ $(p_arrow) '
 }
 
 prompt_setup "$@"
+# Randomm Quotes in Color upon opening the terminal
+# fortune -a | fmt -80 -s | $(shuf -n 1 -e cowsay cowthink) -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n | lolcat
